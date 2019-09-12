130acd33bbe70a7b216a5663bd8e46a061c5dc61
Justin Rosner <rosnej1@mcmaster.ca>
2019-09-12 13:07:24 -0400

    Minor tweaks to change to three lanes and remove existing blocks

--- road_display.py
+++ road_display.py
@@ -1,3 +1,7 @@
+'''
+This is the GUI/Representation of an adaptive cruise control algorithm
+'''
+
 import pygame
 import random
 
@@ -7,209 +11,216 @@ WHITE = (255, 255, 255)
 GREY = (159, 163, 168)
 GREEN = (0, 255, 0)
 RED = (255, 0, 0)
-CAR_COLOR = (181, 230, 29)
 TEXT_COLOR = (250, 105, 10)
 
 
 class Car:
-    def __init__(self, x=0, y=0, dx=4, dy=0, width=30, height=30, color=RED):
+    '''
+    This is the general class for cars that we will use to describe both our car
+    and the surrounding cars on the road
+    '''
+
+    def __init__(self, x_pos=0, y_pos=0, d_x=4, d_y=0, width=30, height=30):
         self.image = ""
-        self.x = x
-        self.y = y
-        self. dx = dx
-        self.dy = dy
+        self.x_pos = x_pos
+        self.y_pos = y_pos
+        self.d_x = d_x
+        self.d_y = d_y
         self.width = width
         self.height = height
-        self.color = color
 
     def load_image(self, img):
+        '''
+        Function to load the image we are going to use for the car
+        '''
+
         self.image = pygame.image.load(img).convert()
         self.image = pygame.transform.scale(self.image, (110, 191))
         self.image.set_colorkey(BLACK)
 
     def draw_image(self):
-        screen.blit(self.image, [self.x, self.y])
+        '''
+        Draw the image of the car to the screen
+        '''
+
+        screen.blit(self.image, [self.x_pos, self.y_pos])
 
     def move_x(self):
-        self.x += self.dx
+        '''
+        Function to move the car along the x-axis
+        '''
+
+        self.x_pos += self.d_x
 
     def move_y(self):
-        self.y += self.dy
+        '''
+        Function to move the car along the y-axis
+        '''
 
-    def draw_rect(self):
-        pygame.draw.rect(screen, self.color,
-                         [self.x, self.y, self.width, self.height], 0)
+        self.y_pos += self.d_y
 
     def check_out_of_screen(self):
-        if self.x+self.width > 600 or self.x < 0:
-            self.x -= self.dx
+        '''
+        Function to see if the car has moved off of the visible screen
+        '''
+
+        if self.x_pos + self.width > 600 or self.x_pos < 0:
+            self.x_pos -= self.d_x
 
 
 def check_collision(player_x, player_y, player_width, player_height, car_x,
                     car_y, car_width, car_height):
-    if (player_x+player_width > car_x) and (player_x < car_x+car_width) and \
-       (player_y < car_y+car_height) and (player_y+player_height > car_y):
-        return True
-    else:
-        return False
+    '''
+    Function to check if two cars have crashed (coordinates overlap)
+    '''
+
+    return bool((player_x+player_width > car_x) and (player_x < car_x+car_width) and \
+                (player_y < car_y+car_height) and (player_y+player_height > car_y))
 
 
-pygame.init()
+def draw_main_menu(screen, size, text_title, text_ins):
+    '''
+    Function to draw the main menu for the app/demo
+    '''
 
-# Set the width and height of the screen [width, height]
-size = (600, 700)
-screen = pygame.display.set_mode(size)
-pygame.display.set_caption("EcoCAR DEV Challenge")
-
-# Loop until the user clicks the close button.
-done = False
-
-# Used to manage how fast the screen updates
-clock = pygame.time.Clock()
-
-# Create a player car object
-player = Car(175, 475, 0, 0, 110, 191, RED)
-player.load_image("chevy.png")
-
-collision = True
-
-# Store the score
-score = 0
-
-# Load the fonts
-font_40 = pygame.font.SysFont("Arial", 40, True, False)
-font_30 = pygame.font.SysFont("Arial", 30, True, False)
-text_title = font_40.render("Ride the Road", True, TEXT_COLOR)
-text_ins = font_30.render("Click to Play!", True, TEXT_COLOR)
-
-
-def draw_main_menu():
     screen.blit(text_title, [size[0] / 2 - 106, size[1] / 2 - 100])
-    score_text = font_40.render("Score: " + str(score), True, TEXT_COLOR)
-    screen.blit(score_text, [size[0] / 2 - 70, size[1] / 2 - 30])
     screen.blit(text_ins, [size[0] / 2 - 85, size[1] / 2 + 40])
     pygame.display.flip()
 
+def main():
+    # Loop until the user clicks the close button.
+    done = False
 
-# Setup signal buttons
-#left_signal = pygame.Rect(screen, (0,0,240),(150,90,100,50))
-right_signal =  pygame.Rect(0, 0, 0, 0)
+    # Used to manage how fast the screen updates
+    clock = pygame.time.Clock()
 
-# Setup the enemy cars
-cars = []
-car_count = 2
-for i in range(car_count):
-    x = random.randrange(0, 340)
-    car = Car(x, random.randrange(-150, -50), 0,
-              random.randint(5, 10), 60, 60, CAR_COLOR)
-    cars.append(car)
+    # Create a player car object
+    player = Car(175, 475, 0, 0, 110, 191)
+    player.load_image("chevy.png")
 
-# Setup the stripes.
-stripes = []
-stripe_count = 20
-stripe_x = 185
-stripe_y = -10
-stripe_width = 20
-stripe_height = 80
-space = 20
+    collision = True
 
-for i in range(stripe_count):
-    stripes.append([185, stripe_y])
-    stripes.append([400, stripe_y])
-    stripe_y += stripe_height + space
+    # Load the fonts
+    font_40 = pygame.font.SysFont("Arial", 40, True, False)
+    font_30 = pygame.font.SysFont("Arial", 30, True, False)
+    text_title = font_40.render("EcoCAR DEV Challenge", True, TEXT_COLOR)
+    text_ins = font_30.render("Click to Run!", True, TEXT_COLOR)
 
-# Main program loop
-while not done:
-    # Main event loop
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            done = True
+    # Setup signal buttons
+    # left_signal = pygame.Rect(screen, (0,0,240),(150,90,100,50))
+    right_signal = pygame.Rect(0, 0, 0, 0)
 
-        # Reset everything when the user starts the game.
-        if collision and event.type == pygame.MOUSEBUTTONDOWN:
-            collision = False
-            for i in range(car_count):
-                cars[i].y = random.randrange(-150, -50)
-                cars[i].x = random.randrange(0, 350)
-            player.x = 175
-            player.dx = 0
-            score = 0
-            pygame.mouse.set_visible(True)
+    # Setup the stripes.
+    stripes = []
+    stripe_count = 20
+    stripe_x = 185
+    stripe_y = -10
+    stripe_width = 20
+    stripe_height = 80
+    space = 20
 
-        if not collision:
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_RIGHT:
-                    player.dx = 4
-                elif event.key == pygame.K_LEFT:
-                    player.dx = -4
+    for i in range(stripe_count):
+        stripes.append([185, stripe_y])
+        stripes.append([400, stripe_y])
+        stripe_y += stripe_height + space
 
-            if event.type == pygame.KEYUP:
-                if event.key == pygame.K_LEFT:
-                    player.dx = 0
-                elif event.key == pygame.K_RIGHT:
-                    player.dx = 0
-            
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                mouse_pos = event.pos # Gets the mouse position
-                if right_signal.collidepoint(mouse_pos):
-                    print("Nice the button works")
-            
+    # Main program loop
+    while not done:
+        # Main event loop
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                done = True
 
-    # --- Game logic should go here
-
-    # --- Screen-clearing code goes here
-    screen.fill(GREY)
-
-    # --- Drawing code should go here
-    if not collision:
-        # Draw the stripes
-        for i in range(stripe_count):
-            pygame.draw.rect(screen, WHITE, [stripes[i][0], stripes[i][1],
-                             stripe_width, stripe_height])
-
-        # Move the stripes
-        for i in range(stripe_count):
-            stripes[i][1] += 3
-            if stripes[i][1] > size[1]:
-                stripes[i][1] = -40 - stripe_height
-
-        player.draw_image()
-        player.move_x()
-        player.check_out_of_screen()
-
-        # Check if the enemy cars move out of the screen.
-        for i in range(car_count):
-            cars[i].draw_rect()
-            cars[i].y += cars[i].dy
-            if cars[i].y > size[1]:
-                score += 10
-                cars[i].y = random.randrange(-150, -50)
-                cars[i].x = random.randrange(0, 340)
-                cars[i].dy = random.randint(4, 9)
-
-        # Check the collision of the player with the car
-        for i in range(car_count):
-            if check_collision(player.x, player.y, player.width, player.height,
-                               cars[i].x, cars[i].y, cars[i].width,
-                               cars[i].height):
-                collision = True
+            # Reset everything when the user starts the game.
+            if collision and event.type == pygame.MOUSEBUTTONDOWN:
+                collision = False
+                player.x_pos = 175
+                player.d_x = 0
+                score = 0
                 pygame.mouse.set_visible(True)
-                break
 
-        # Draw the score.
-        txt_score = font_30.render("Score: "+str(score), True, WHITE)
-        screen.blit(txt_score, [15, 15])
+            if not collision:
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_RIGHT:
+                        player.d_x = 4
+                    elif event.key == pygame.K_LEFT:
+                        player.d_x = -4
 
-        pygame.display.flip()
+                if event.type == pygame.KEYUP:
+                    if event.key == pygame.K_LEFT:
+                        player.d_x = 0
+                    elif event.key == pygame.K_RIGHT:
+                        player.d_x = 0
 
-        # Draw the rect (THIS IS JUST A TEST)
-        pygame.draw.rect(screen, [255, 0, 0], right_signal)
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    mouse_pos = event.pos  # Gets the mouse position
+                    if right_signal.collidepoint(mouse_pos):
+                        print("Nice the button works")
 
-    else:
-        draw_main_menu()
+        # --- Game logic should go here
 
-    # --- Limit to 60 frames per second
-    clock.tick(60)
+        # --- Screen-clearing code goes here
+        screen.fill(GREY)
 
-# Close the window and quit.
-pygame.quit()
+        # --- Drawing code should go here
+        if not collision:
+            # Draw the stripes
+            for i in range(stripe_count):
+                pygame.draw.rect(screen, WHITE, [stripes[i][0], stripes[i][1],
+                                                stripe_width, stripe_height])
+
+            # Move the stripes
+            for i in range(stripe_count):
+                stripes[i][1] += 3
+                if stripes[i][1] > size[1]:
+                    stripes[i][1] = -40 - stripe_height
+
+            player.draw_image()
+            player.move_x()
+            player.check_out_of_screen()
+
+            '''
+            We will have to come back to this stuff later
+
+            # Check if the other cars move out of the screen.
+            for i in range(car_count):
+                cars[i].draw_rect()
+                cars[i].y_pos += cars[i].d_y
+                if cars[i].y_pos > size[1]:
+                    score += 10
+                    cars[i].y_pos = random.randrange(-150, -50)
+                    cars[i].x_pos = random.randrange(0, 340)
+                    cars[i].d_y = random.randint(4, 9)
+
+
+            # Check the collision of the player with the car
+            for i in range(car_count):
+                if check_collision(player.x_pos, player.y_pos, player.width, player.height,
+                                cars[i].x_pos, cars[i].y_pos, cars[i].width,
+                                cars[i].height):
+                    collision = True
+                    pygame.mouse.set_visible(True)
+                    break
+
+            '''
+            pygame.display.flip()
+
+            # Draw the rect (THIS IS JUST A TEST)
+            pygame.draw.rect(screen, [0, 0, 0], right_signal)
+
+        else:
+            draw_main_menu(screen, size, text_title, text_ins)
+
+        # Limiting to 60 frames per second
+        clock.tick(60)
+
+if __name__ == '__main__':
+    pygame.init()
+
+    size = (600, 700)
+    screen = pygame.display.set_mode(size)
+    pygame.display.set_caption("EcoCAR DEV Challenge")
+
+    main()
+    pygame.quit()
+
